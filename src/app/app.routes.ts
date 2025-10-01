5478e4c23df060f0e98b7bb5ae92da77de8227f3
Robert Vandelinder <robjvan@gmail.com>
2025-09-30 22:32:52 -0230

    Added basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { NoContentPage } from './pages/no-content-page/no-content-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: NoContentPage,
+  },
+];
