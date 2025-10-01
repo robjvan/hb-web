e8d97e115ab225414ba780ede9d24c436a4cc5ab
Robert Vandelinder <robjvan@gmail.com>
2025-09-30 22:33:03 -0230

    Added endpoint constants

---
+++ src/app/constants.ts
@@ -0,0 +1,6 @@
+export const SERVER_URL = 'http://hb-server.robjvan.ca';
+
+export const API_ENDPOINT = 'api/v1';
+export const PING_ENDPOING = 'api/v1/ping';
+export const HAIKU_ENDPOINT = 'api/v1/haiku';
+// export const LOGGING_ENDPOINT = 'api/v1/logging';
