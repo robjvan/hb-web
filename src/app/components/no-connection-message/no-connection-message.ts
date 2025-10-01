9e799b597160c0e4924c0649d0d5320e36d199a7
Robert Vandelinder <robjvan@gmail.com>
2025-09-30 22:31:32 -0230

    Added NoConnectionMessge component

---
+++ src/app/components/no-connection-message/no-connection-message.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-connection-message',
+  templateUrl: './no-connection-message.html',
+  styleUrl: './no-connection-message.css',
+})
+export class NoConnectionMessage {}
